ed57b281ef7f346181d87662361a3db673785f05
Megaemce <1651451+Megaemce@users.noreply.github.com>
2023-12-20 12:24:14 +0700

    add better jsdoc, rename args, simplify code

--- src/render/basic/designer.ts
+++ src/render/basic/designer.ts
@@ -11,7 +11,7 @@ const MOUNTAIN_COVER_THRESHOLD = 0.3;
 /**
  * Radius of the circular area used to check for local maxima.
  */
-const MOUNTAIN_RADIUS = 2;
+const MOUNTAIN_RADIUS = 2; //
 
 /**
  * Interval at which distant mountains are generated.
@@ -29,22 +29,39 @@ const FLAT_MOUNTAIN_PROBABILITY = 0.01;
 const BOAT_PROBABILITY = 0.2;
 
 /**
- * Checks if a point is a local maximum within a circular area.
- * @param point - The point to check.
- * @param f - The function to evaluate.
- * @param radius - The radius of the circular area.
- * @returns True if the point is a local maximum, false otherwise.
+ * Step size along the x-axis for generating terrain.
  */
-function localMax(
-    point: Point,
-    f: (point: Point) => number,
+const X_STEP = 5;
+
+/**
+ * Width of the mountains in the design.
+ */
+const MOUNTAIN_WIDTH = 200;
+
+/**
+ * Sample value for the noise function.
+ */
+const NOISE_SAMPLE = 0.03;
+
+/**
+ * Checks if a point is a local maximum within a circular area.
+ * @param {Point} center - The center point to check.
+ * @param {Function} getValue - The function to get the value at a given point.
+ * @param {number} radius - The radius of the circular area.
+ * @returns {boolean} True if the center point is a local maximum, false otherwise.
+ */
+function isLocalMaximum(
+    center: Point,
+    getValue: Function,
     radius: number
 ): boolean {
-    const z0 = f(point);
+    const centerValue = getValue(center);
 
-    for (let x = point.x - radius; x < point.x + radius; x++) {
-        for (let y = point.y - radius; y < point.y + radius; y++) {
-            if (z0 < f(new Point(x, y))) {
+    for (let x = center.x - radius; x <= center.x + radius; x++) {
+        for (let y = center.y - radius; y <= center.y + radius; y++) {
+            const neighborValue = getValue(new Point(x, y));
+
+            if (centerValue < neighborValue) {
                 return false;
             }
         }
@@ -55,10 +72,10 @@ function localMax(
 
 /**
  * Checks if a chunk needs to be added to the region.
- * @param region - The region of existing chunks.
- * @param chunk - The chunk to check.
- * @param radius - The threshold radius for considering chunks to be the same.
- * @returns True if the chunk needs to be added, false otherwise.
+ * @param {IChunk[]} region - The region of existing chunks.
+ * @param {IChunk} chunk - The chunk to check.
+ * @param {number} radius - The threshold radius for considering chunks to be the same.
+ * @returns {boolean} True if the chunk needs to be added, false otherwise.
  */
 function needsAdding(
     region: IChunk[],
@@ -72,54 +89,50 @@ function needsAdding(
 
 /**
  * Generates mountain chunks based on Perlin noise.
- * @param prng - The pseudorandom number generator.
- * @param noiseFunction - The noise function.
- * @param yRange - The y range function.
- * @param mountainCover - The array to track mountain coverage.
- * @param xStep - The step size along the x-axis.
- * @param iMin - The minimum index for generation.
- * @param iMax - The maximum index for generation.
- * @param xOffset - The offset along the x-axis.
- * @param width - The width of the mountains.
- * @returns An array of generated chunks.
+ * @param {PRNG} prng - The pseudorandom number generator.
+ * @param {Function} noiseFunction - The noise function.
+ * @param {Function} yRange - The y range function.
+ * @param {number[]} mountainCover - The array to track mountain coverage.
+ * @param {number} iMin - The minimum index for generation.
+ * @param {number} iMax - The maximum index for generation.
+ * @param {number} xOffset - The offset along the x-axis.
+ * @returns {IChunk[]} An array of generated chunks.
  */
 function generateMountainChunks(
     prng: PRNG,
     noiseFunction: (point: Point) => number,
     yRange: (x: number) => number,
     mountainCover: number[],
-    xStep: number,
     iMin: number,
     iMax: number,
-    xOffset: number,
-    width: number
+    xOffset: number
 ): IChunk[] {
     const region: IChunk[] = [];
 
     for (let i = iMin; i < iMax; i++) {
-        const x = i * xStep + xOffset;
+        const x = i * X_STEP + xOffset;
 
-        for (let j = 0; j < yRange(x) * 480; j += 30) {
+        for (let y = 0; y < yRange(x) * 480; y += 30) {
             if (
-                noiseFunction(new Point(x, j)) > MOUNTAIN_COVER_THRESHOLD &&
-                localMax(new Point(x, j), noiseFunction, MOUNTAIN_RADIUS)
+                noiseFunction(new Point(x, y)) > MOUNTAIN_COVER_THRESHOLD &&
+                isLocalMaximum(new Point(x, y), noiseFunction, MOUNTAIN_RADIUS)
             ) {
-                const xOf = x + prng.random(-500, 500);
-                const yOf = j + 300;
+                const xOffset = x + prng.random(-500, 500);
+                const yOffset = y + 300;
 
                 const mountainChunk = new DesignChunk(
                     "mount",
-                    xOf,
-                    yOf,
-                    noiseFunction(new Point(x, j))
+                    xOffset,
+                    yOffset,
+                    noiseFunction(new Point(x, y))
                 );
 
                 if (needsAdding(region, mountainChunk)) {
                     region.push(mountainChunk);
 
                     for (
-                        let k = Math.floor((xOf - width) / xStep);
-                        k < (xOf + width) / xStep;
+                        let k = Math.floor((xOffset - MOUNTAIN_WIDTH) / X_STEP);
+                        k < (xOffset + MOUNTAIN_WIDTH) / X_STEP;
                         k++
                     ) {
                         mountainCover[k] = isNaN(mountainCover[k])
@@ -130,7 +143,7 @@ function generateMountainChunks(
             }
         }
 
-        if (Math.abs(x) % DIST_MOUNTAIN_INTERVAL < Math.max(1, xStep - 1)) {
+        if (Math.abs(x) % DIST_MOUNTAIN_INTERVAL < Math.max(1, X_STEP - 1)) {
             const distMountainChunk = new DesignChunk(
                 "distmount",
                 x,
@@ -149,18 +162,16 @@ function generateMountainChunks(
 
 /**
  * Generates flat mountain chunks based on Perlin noise.
- * @param prng - The pseudorandom number generator.
- * @param noiseFunction - The noise function.
- * @param xStep - The step size along the x-axis.
- * @param iMin - The minimum index for generation.
- * @param iMax - The maximum index for generation.
- * @param xOffset - The offset along the x-axis.
- * @returns An array of generated chunks.
+ * @param {PRNG} prng - The pseudorandom number generator.
+ * @param {Function} noiseFunction - The noise function.
+ * @param {number} iMin - The minimum index for generation.
+ * @param {number} iMax - The maximum index for generation.
+ * @param {number} xOffset - The offset along the x-axis.
+ * @returns {IChunk[]} An array of generated chunks.
  */
 function generateFlatMountainChunks(
     prng: PRNG,
     noiseFunction: (point: Point) => number,
-    xStep: number,
     iMin: number,
     iMax: number,
     xOffset: number
@@ -168,7 +179,7 @@ function generateFlatMountainChunks(
     const region: IChunk[] = [];
 
     for (let i = iMin; i < iMax; i++) {
-        const x = i * xStep + xOffset;
+        const x = i * X_STEP + xOffset;
 
         if (prng.random() < FLAT_MOUNTAIN_PROBABILITY) {
             for (let j = 0; j < prng.random(0, 4); j++) {
@@ -191,16 +202,14 @@ function generateFlatMountainChunks(
 
 /**
  * Generates boat chunks based on Perlin noise.
- * @param prng - The pseudorandom number generator.
- * @param xStep - The step size along the x-axis.
- * @param iMin - The minimum index for generation.
- * @param iMax - The maximum index for generation.
- * @param xOffset - The offset along the x-axis.
- * @returns An array of generated chunks.
+ * @param {PRNG} prng - The pseudorandom number generator.
+ * @param {number} iMin - The minimum index for generation.
+ * @param {number} iMax - The maximum index for generation.
+ * @param {number} xOffset - The offset along the x-axis.
+ * @returns {IChunk[]} An array of generated chunks.
  */
 function generateBoatChunks(
     prng: PRNG,
-    xStep: number,
     iMin: number,
     iMax: number,
     xOffset: number
@@ -209,7 +218,7 @@ function generateBoatChunks(
 
     for (let i = iMin; i < iMax; i++) {
         if (prng.random() < BOAT_PROBABILITY) {
-            const x = i * xStep + xOffset;
+            const x = i * X_STEP + xOffset;
             const boatChunk = new DesignChunk("boat", x, prng.random(300, 690));
 
             if (needsAdding(region, boatChunk, 400)) {
@@ -223,11 +232,11 @@ function generateBoatChunks(
 
 /**
  * Generates terrain design chunks based on Perlin noise.
- * @param prng - The pseudorandom number generator.
- * @param mountainCover - The array to track mountain coverage.
- * @param xMin - The minimum x-coordinate for generation.
- * @param xMax - The maximum x-coordinate for generation.
- * @returns An array of generated design chunks.
+ * @param {PRNG} prng - The pseudorandom number generator.
+ * @param {number[]} mountainCover - The array to track mountain coverage.
+ * @param {number} xMin - The minimum x-coordinate for generation.
+ * @param {number} xMax - The maximum x-coordinate for generation.
+ * @returns {IChunk[]} An array of generated design chunks.
  */
 export function design(
     prng: PRNG,
@@ -236,17 +245,12 @@ export function design(
     xMax: number
 ): IChunk[] {
     const region: IChunk[] = [];
-    const samp = 0.03;
-    const noiseFunction = (point: Point) =>
-        Math.max(Noise.noise(prng, point.x * samp) - 0.55, 0) * 2;
+    const iMin = Math.floor(xMin / X_STEP);
+    const iMax = Math.floor(xMax / X_STEP);
+    const xOffset = (xMin % X_STEP) + (xMin < 0 ? 1 : 0) * X_STEP;
     const yRange = (x: number) => Noise.noise(prng, x * 0.01, Math.PI);
-
-    const xStep = 5;
-    const width = 200;
-
-    const iMin = Math.floor(xMin / xStep);
-    const iMax = Math.floor(xMax / xStep);
-    const xOffset = (xMin % xStep) + (xMin < 0 ? 1 : 0) * xStep;
+    const noiseFunction = (point: Point) =>
+        Math.max(Noise.noise(prng, point.x * NOISE_SAMPLE) - 0.55, 0) * 2;
 
     for (let i = iMin; i < iMax; i++) {
         if (isNaN(mountainCover[i])) mountainCover[i] = 0;
@@ -258,24 +262,15 @@ export function design(
             noiseFunction,
             yRange,
             mountainCover,
-            xStep,
-            iMin,
-            iMax,
-            xOffset,
-            width
-        )
-    );
-    region.push(
-        ...generateFlatMountainChunks(
-            prng,
-            noiseFunction,
-            xStep,
             iMin,
             iMax,
             xOffset
         )
     );
-    region.push(...generateBoatChunks(prng, xStep, iMin, iMax, xOffset));
+    region.push(
+        ...generateFlatMountainChunks(prng, noiseFunction, iMin, iMax, xOffset)
+    );
+    region.push(...generateBoatChunks(prng, iMin, iMax, xOffset));
 
     return region;
 }
