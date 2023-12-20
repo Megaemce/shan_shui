d169734805743ec5e2f18679dc8df7eb0f558df7
Megaemce <1651451+Megaemce@users.noreply.github.com>
2023-12-20 10:08:35 +0700

    update scrollablecanvas with modern react code

--- src/ui/ScrollableCanvas.tsx
+++ src/ui/ScrollableCanvas.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { PRNG } from "../render/basic/PRNG";
 import { Range } from "../render/basic/range";
 import { ChunkCache } from "../render/chunkCache";
@@ -11,41 +11,26 @@ interface IBarProps {
     icon: string;
 }
 
-interface IBarState {
-    isHover: boolean;
-}
+const ScrollBar: React.FC<IBarProps> = ({ id, height, onClick, icon }) => {
+    const [isHover, setIsHover] = useState(false);
 
-class ScrollBar extends React.Component<IBarProps, IBarState> {
-    constructor(props: IBarProps) {
-        super(props);
-        this.state = {
-            isHover: false,
-        };
-    }
-
-    render() {
-        const onMouseOver = () => this.setState({ isHover: true });
-        const onMouseOut = () => this.setState({ isHover: false });
-        const bgrColor = this.state.isHover ? 0.1 : 0;
-
-        return (
-            <div
-                id={this.props.id}
-                onMouseOver={onMouseOver}
-                onMouseOut={onMouseOut}
-                onClick={this.props.onClick}
-                style={{
-                    backgroundColor: `rgba(0, 0, 0, ${bgrColor})`,
-                    height: this.props.height,
-                }}
-            >
-                <div id={`${this.props.id}.t`}>
-                    <span>{this.props.icon}</span>
-                </div>
+    return (
+        <div
+            id={id}
+            onMouseOver={() => setIsHover(true)}
+            onMouseOut={() => setIsHover(false)}
+            onClick={onClick}
+            style={{
+                backgroundColor: `rgba(0, 0, 0, ${isHover ? 0.1 : 0})`,
+                height,
+            }}
+        >
+            <div id={`${id}.t`}>
+                <span>{icon}</span>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 interface IProps {
     xscroll: (v: number) => void;
@@ -59,82 +44,82 @@ interface IProps {
     chunkCache: ChunkCache;
 }
 
-class ScrollableCanvas extends React.Component<IProps> {
-    static id = "SCROLLABLE_CANVAS";
-    cwid: number = 512;
-    canv: string = "";
-    zoom: number = 1.142;
+const ScrollableCanvas: React.FC<IProps> = ({
+    xscroll,
+    windy,
+    background,
+    seed,
+    cursx,
+    windx,
+    updateflag,
+    prng,
+    chunkCache,
+}) => {
+    const cwid = 512;
+    const zoom = 1.142;
 
-    render() {
-        const { prng, chunkCache } = this.props;
+    const viewbox = `${cursx} 0 ${windx / zoom} ${windy / zoom}`;
+    const nr = new Range(cursx, cursx + windx);
+    chunkCache.update(prng, nr, cwid);
 
-        const xscroll = this.props.xscroll;
-        const viewbox = `${this.props.cursx} 0 ${
-            this.props.windx / this.zoom
-        } ${this.props.windy / this.zoom}`;
-        const nr = new Range(
-            this.props.cursx,
-            this.props.cursx + this.props.windx
-        );
-        chunkCache.update(prng, nr, this.cwid);
+    const tableId = "SCROLLABLE_CANVAS"; // Unique id for the table
 
-        return (
-            <table id={ScrollableCanvas.id}>
-                <tbody>
-                    <tr>
-                        <td>
-                            <ScrollBar
-                                id="L"
-                                onClick={() => xscroll(-200)}
-                                height={this.props.windy - 8}
-                                icon="&#x3008;"
-                            />
-                        </td>
-                        <td>
-                            <div
-                                id="BG"
-                                style={{
-                                    backgroundImage: `url(${this.props.background})`,
-                                    width: this.props.windx,
-                                    height: this.props.windy,
-                                    left: 0,
-                                    position: "fixed",
-                                    top: 0,
-                                }}
+    return (
+        <table id={tableId}>
+            <tbody>
+                <tr>
+                    <td>
+                        <ScrollBar
+                            id="L"
+                            onClick={() => xscroll(-200)}
+                            height={windy - 8}
+                            icon="&#x3008;"
+                        />
+                    </td>
+                    <td>
+                        <div
+                            id="BG"
+                            style={{
+                                backgroundImage: `url(${background})`,
+                                width: windx,
+                                height: windy,
+                                left: 0,
+                                position: "fixed",
+                                top: 0,
+                            }}
+                        >
+                            <svg
+                                id="SVG"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width={windx}
+                                height={windy}
+                                style={{ mixBlendMode: "multiply" }}
+                                viewBox={viewbox}
                             >
-                                <svg
-                                    id="SVG"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width={this.props.windx}
-                                    height={this.props.windy}
-                                    style={{ mixBlendMode: "multiply" }}
-                                    viewBox={viewbox}
-                                >
-                                    {chunkCache.chunks.map((c) => (
-                                        <g
-                                            key={`${c.tag} ${c.x} ${c.y}`}
-                                            transform="translate(0, 0)"
-                                            dangerouslySetInnerHTML={{
-                                                __html: c.render(),
-                                            }}
-                                        ></g>
-                                    ))}
-                                </svg>
-                            </div>
-                        </td>
-                        <td>
-                            <ScrollBar
-                                id="R"
-                                onClick={() => xscroll(200)}
-                                height={this.props.windy - 8}
-                                icon="&#x3009;"
-                            />
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        );
-    }
-}
+                                {chunkCache.chunks.map((c) => (
+                                    <g
+                                        key={`${c.tag} ${c.x} ${c.y}`}
+                                        transform="translate(0, 0)"
+                                        dangerouslySetInnerHTML={{
+                                            __html: c.render(),
+                                        }}
+                                    ></g>
+                                ))}
+                            </svg>
+                        </div>
+                    </td>
+                    <td>
+                        <ScrollBar
+                            id="R"
+                            onClick={() => xscroll(200)}
+                            height={windy - 8}
+                            icon="&#x3009;"
+                        />
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    );
+};
 
 export default ScrollableCanvas;
