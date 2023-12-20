a3d37ca92d8cbfa8e3eb652f3f242cb0c980bc06
Megaemce <1651451+Megaemce@users.noreply.github.com>
2023-12-20 11:07:15 +0700

    rename var names and add jsdoc comments

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,29 @@ import { PerlinNoise } from "./render/basic/perlinNoise";
 import { ChunkCache } from "./render/chunkCache";
 import "./App.css";
 
+/**
+ * Main application component.
+ * @component
+ * @returns {JSX.Element} The main application component.
+ */
 const App: React.FC = () => {
+    const FPS = 60;
+    const currentSeed = new URLSearchParams(window.location.search).get("seed");
+    const currentDate = new Date().getTime().toString();
+
+    // Refs for accessing child components
     const bgRenderRef = useRef<BackgroundRender>(null);
-    const [seed, setSeed] = useState<string>(
-        new URLSearchParams(window.location.search).get("seed") ||
-            new Date().getTime().toString()
+    const prngRef = useRef(new PRNG());
+    const noiseRef = useRef(new PerlinNoise());
+    const chunkCacheRef = useRef(new ChunkCache());
+
+    // State variables
+    const [seed, setSeed] = useState<string>(currentSeed || currentDate);
+    const [currentPosition, setCurrentPosition] = useState<number>(0);
+    const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+    const [windowHeight, setWindowHeight] = useState<number>(
+        window.innerHeight
     );
-    const [cursX, setCursX] = useState<number>(0);
-    const [windX, setWindX] = useState<number>(window.innerWidth);
-    const [windY, setWindY] = useState<number>(window.innerHeight);
     const [autoLoad, setAutoLoad] = useState<boolean>(false);
     const [saveRange, setSaveRange] = useState<Range>(
         new Range(0, window.innerWidth)
@@ -26,15 +40,51 @@ const App: React.FC = () => {
         undefined
     );
 
-    const prngRef = useRef(new PRNG());
-    const noiseRef = useRef(new PerlinNoise());
-    const chunkCacheRef = useRef(new ChunkCache());
-    const FPS = 2;
+    /**
+     * Callback function to handle changes in the PRNG seed.
+     * @param {string} newSeed - The new seed value.
+     */
+    const changeSeed = (newSeed: string) => {
+        setSeed(newSeed);
+    };
 
+    /**
+     * Callback function to handle changes in the save range.
+     * @param {Range} newSaveRange - The new save range.
+     */
+    const onChangeSaveRange = (newSaveRange: Range) => {
+        setSaveRange(newSaveRange);
+    };
+
+    /**
+     * Callback function to toggle auto-scrolling.
+     * @param {boolean} isEnabled - Indicates whether auto-scroll is enabled.
+     * @param {number} step - The step value for auto-scrolling.
+     */
+    const toggleAutoScroll = (isEnabled: boolean, step: number) => {
+        setAutoScroll(isEnabled);
+        horizonalAutoScroll(step);
+    };
+
+    /**
+     * Callback function to toggle auto-loading.
+     * @param {boolean} isEnabled - Indicates whether auto-load is enabled.
+     */
+    const toggleAutoLoad = (isEnabled: boolean) => {
+        setAutoLoad(isEnabled);
+        setSaveRange(new Range(currentPosition, currentPosition + windowWidth));
+    };
+
+    /**
+     * Effect to handle changes in the PRNG seed.
+     */
     useEffect(() => {
         prngRef.current.seed(seed);
     }, [seed]);
 
+    /**
+     * Effect to update the background image and handle window resize events.
+     */
     useEffect(() => {
         const url = bgRenderRef.current?.generate(
             prngRef.current,
@@ -43,8 +93,8 @@ const App: React.FC = () => {
         setBackgroundImage(url);
 
         const resizeCallback = () => {
-            setWindX(window.innerWidth);
-            setWindY(window.innerHeight);
+            setWindowWidth(window.innerWidth);
+            setWindowHeight(window.innerHeight);
         };
 
         window.addEventListener("resize", resizeCallback);
@@ -54,54 +104,58 @@ const App: React.FC = () => {
         };
     }, [prngRef, noiseRef, seed]);
 
+    /**
+     * Callback function to handle horizontal scrolling.
+     * @param {number} value - The scroll value.
+     */
     const horizontalScroll = useCallback(
-        (v: number) => {
-            const nextX = cursX + v;
-            setCursX(nextX);
+        (value: number) => {
+            const nextPosition = currentPosition + value;
+            setCurrentPosition(nextPosition);
 
             if (autoLoad) {
-                setSaveRange(new Range(nextX, nextX + windX));
+                setSaveRange(
+                    new Range(nextPosition, nextPosition + windowWidth)
+                );
             }
         },
-        [autoLoad, cursX, windX]
+        [autoLoad, currentPosition, windowWidth]
     );
 
-    const autoXScroll = useCallback(
-        (v: number) => {
-            if (autoScroll) {
-                horizontalScroll(v / FPS);
-                const autoXScrollCallback = (v: number) => autoXScroll(v);
-                setTimeout(() => autoXScrollCallback(v), 1000 / FPS);
-            }
+    /**
+     * Callback function to handle horizontal auto-scrolling.
+     * @param {number} value - The scroll value.
+     */
+    const horizonalAutoScroll = useCallback(
+        (value: number) => {
+            if (!autoScroll) return;
+
+            const autoScrollCallback = () => {
+                horizontalScroll(value / FPS);
+                requestAnimationFrame(autoScrollCallback);
+            };
+
+            const interval = FPS;
+            const autoScrollTimeout = setTimeout(autoScrollCallback, interval);
+
+            return () => clearTimeout(autoScrollTimeout);
         },
         [autoScroll, FPS, horizontalScroll]
     );
 
+    /**
+     * Effect to initiate horizontal auto-scrolling.
+     */
     useEffect(() => {
-        autoXScroll(FPS);
-    }, [autoXScroll, FPS]);
+        horizonalAutoScroll(FPS);
+    }, [horizonalAutoScroll, FPS]);
 
-    const reloadWithSeed = () => {
+    /**
+     * Callback function to reload the page with a new seed.
+     */
+    const reloadWindowSeed = () => {
         const url = window.location.href.split("?")[0];
         window.location.href = `${url}?seed=${seed}`;
-    };
-
-    const changeSeed = (newSeed: string) => {
-        setSeed(newSeed);
-    };
-
-    const onChangeSaveRange = (newSaveRange: Range) => {
-        setSaveRange(newSaveRange);
-    };
-
-    const toggleAutoScroll = (isAutoScroll: boolean, step: number) => {
-        setAutoScroll(isAutoScroll);
-        autoXScroll(step);
-    };
-
-    const toggleAutoLoad = (shouldAutoLoad: boolean) => {
-        setAutoLoad(shouldAutoLoad);
-        setSaveRange(new Range(cursX, cursX + windX));
     };
 
     return (
@@ -110,13 +164,13 @@ const App: React.FC = () => {
                 <SettingPanel
                     seed={seed}
                     changeSeed={changeSeed}
-                    reloadWSeed={reloadWithSeed}
+                    reloadWindowSeed={reloadWindowSeed}
                     horizontalScroll={horizontalScroll}
                     toggleAutoScroll={toggleAutoScroll}
-                    cursx={cursX}
+                    currentPosition={currentPosition}
                     chunkCache={chunkCacheRef.current}
-                    windx={windX}
-                    windy={windY}
+                    windowWidth={windowWidth}
+                    windowHeight={windowHeight}
                     prng={prngRef.current}
                     saveRange={saveRange}
                     onChangeSaveRange={onChangeSaveRange}
@@ -124,11 +178,11 @@ const App: React.FC = () => {
                 />
                 <ScrollableCanvas
                     horizontalScroll={horizontalScroll}
-                    windy={windY}
+                    windowHeight={windowHeight}
                     background={backgroundImage}
                     seed={seed}
-                    cursx={cursX}
-                    windx={windX}
+                    currentPosition={currentPosition}
+                    windowWidth={windowWidth}
                     prng={prngRef.current}
                     chunkCache={chunkCacheRef.current}
                 />
