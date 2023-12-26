8f98bef940ac34399ba626334ea788df4616261a
Megaemce <1651451+Megaemce@users.noreply.github.com>
2023-12-26 19:21:37 +0700

    reformat generateBranch

--- src/classes/svgPolylines/generateBranch.ts
+++ src/classes/svgPolylines/generateBranch.ts
@@ -22,69 +22,82 @@ export default function generateBranch(
     bendingAngle: number = 0.2 * Math.PI,
     details: number = 10
 ): Point[][] {
-    let newX = 0;
-    let newY = 0;
-    const tlist = [[newX, newY]];
-    let a0 = 0;
-    const g = 3;
-    for (let i = 0; i < g; i++) {
-        a0 += ((prng.random(1, 2) * bendingAngle) / 2) * prng.randomSign();
-        newX += (Math.cos(a0) * height) / g;
-        newY -= (Math.sin(a0) * height) / g;
-        tlist.push([newX, newY]);
+    // Generate the initial curve points
+    const curvePoints: Point[] = [];
+    let newPoint = new Point(0, 0);
+    let angle0 = 0;
+    const numSegments = 3;
+
+    for (let i = 0; i < numSegments; i++) {
+        angle0 += ((prng.random(1, 2) * bendingAngle) / 2) * prng.randomSign();
+        newPoint.x += (Math.cos(angle0) * height) / numSegments;
+        newPoint.y -= (Math.sin(angle0) * height) / numSegments;
+        curvePoints.push(newPoint);
     }
-    const ta = Math.atan2(
-        tlist[tlist.length - 1][1],
-        tlist[tlist.length - 1][0]
+
+    // Rotate the curve to a specified angle
+    const rotationAngle = Math.atan2(
+        curvePoints[curvePoints.length - 1].y,
+        curvePoints[curvePoints.length - 1].x
     );
+    curvePoints.forEach((point) => {
+        const curveAngle = Math.atan2(point.y, point.x);
+        const distance = Math.sqrt(point.x ** 2 + point.y ** 2);
+        point.x = distance * Math.cos(curveAngle - rotationAngle + angle);
+        point.y = distance * Math.sin(curveAngle - rotationAngle + angle);
+    });
 
-    for (let i = 0; i < tlist.length; i++) {
-        const a = Math.atan2(tlist[i][1], tlist[i][0]);
-        const d = Math.sqrt(
-            tlist[i][0] * tlist[i][0] + tlist[i][1] * tlist[i][1]
-        );
-        tlist[i][0] = d * Math.cos(a - ta + angle);
-        tlist[i][1] = d * Math.sin(a - ta + angle);
-    }
-
+    // Generate two stroke paths based on the curved line
     const trlist1: Point[] = [];
     const trlist2: Point[] = [];
-    const span = details;
-    const tl = (tlist.length - 1) * span;
-    let lx = 0;
-    let ly = 0;
+    const totalPoints = (curvePoints.length - 1) * details;
+    let prevPoint = new Point(0, 0);
 
-    for (let i = 0; i < tl; i += 1) {
-        const lastPoint = tlist[Math.floor(i / span)];
-        const nextPoint = tlist[Math.ceil(i / span)];
-        const p = (i % span) / span;
-        const newX = lastPoint[0] * (1 - p) + nextPoint[0] * p;
-        const newY = lastPoint[1] * (1 - p) + nextPoint[1] * p;
+    for (let i = 0; i < totalPoints; i++) {
+        const lastPoint = curvePoints[Math.floor(i / details)];
+        const nextPoint = curvePoints[Math.ceil(i / details)];
+        const p = (i % details) / details;
+        newPoint.x = lastPoint.x * (1 - p) + nextPoint.x * p;
+        newPoint.y = lastPoint.y * (1 - p) + nextPoint.y * p;
 
-        const angle = Math.atan2(newY - ly, newX - lx);
-        const woff =
+        const angle = Math.atan2(
+            newPoint.y - prevPoint.y,
+            newPoint.x - prevPoint.x
+        );
+        const widthOffset =
             ((Noise.noise(prng, i * 0.3) - 0.5) * strokeWidth * height) / 80;
 
-        let b = 0;
+        let randomness = 0;
         if (p === 0) {
-            b = prng.random() * strokeWidth;
+            randomness = prng.random() * strokeWidth;
         }
 
-        const nw = strokeWidth * (((tl - i) / tl) * 0.5 + 0.5);
+        const newWidth =
+            strokeWidth * (((totalPoints - i) / totalPoints) * 0.5 + 0.5);
+
         trlist1.push(
             new Point(
-                newX + Math.cos(angle + Math.PI / 2) * (nw + woff + b),
-                newY + Math.sin(angle + Math.PI / 2) * (nw + woff + b)
+                newPoint.x +
+                    Math.cos(angle + Math.PI / 2) *
+                        (newWidth + widthOffset + randomness),
+                newPoint.y +
+                    Math.sin(angle + Math.PI / 2) *
+                        (newWidth + widthOffset + randomness)
             )
         );
+
         trlist2.push(
             new Point(
-                newX + Math.cos(angle - Math.PI / 2) * (nw - woff + b),
-                newY + Math.sin(angle - Math.PI / 2) * (nw - woff + b)
+                newPoint.x +
+                    Math.cos(angle - Math.PI / 2) *
+                        (newWidth - widthOffset + randomness),
+                newPoint.y +
+                    Math.sin(angle - Math.PI / 2) *
+                        (newWidth - widthOffset + randomness)
             )
         );
-        lx = newX;
-        ly = newY;
+
+        prevPoint = newPoint;
     }
 
     return [trlist1, trlist2];
