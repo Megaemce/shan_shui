dac744677ef2422afd7ddbbbef60cadcb0c55398
Megaemce <1651451+Megaemce@users.noreply.github.com>
2023-12-27 21:43:40 +0700

    remove seed from App

--- src/App.tsx
+++ src/App.tsx
@@ -170,7 +170,6 @@ export const App: React.FC = () => {
                     horizontalScroll={horizontalScroll}
                     windowHeight={windowHeight}
                     background={backgroundImage}
-                    seed={seed}
                     currentPosition={currentPosition}
                     windowWidth={windowWidth}
                     chunkCache={chunkCacheRef.current}
