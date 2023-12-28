8c696071315d140e9769758ddb2319f3487fb258
Megaemce <1651451+Megaemce@users.noreply.github.com>
2023-12-28 11:04:35 +0700

    Optimize background rendering

--- src/ui/Background.tsx
+++ src/ui/Background.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Perlin from "../classes/Perlin";
 import PRNG from "../classes/PRNG";
 import { config } from "../config";
 
@@ -14,12 +13,10 @@ export default class Background extends React.Component {
 
         for (let i = 0; i < RESOLUTION / 2 + 1; i++) {
             for (let j = 0; j < RESOLUTION / 2 + 1; j++) {
-                let color = 245 + Perlin.noise(i * 0.1, j * 0.1) * 10;
-                color -= PRNG.random(0, 20);
-
-                const red = color.toFixed(0);
-                const green = (color * 0.95).toFixed(0);
-                const blue = (color * 0.85).toFixed(0);
+                const color = PRNG.random(225, 245);
+                const red = Math.round(color);
+                const green = Math.round(color * 0.95);
+                const blue = Math.round(color * 0.85);
 
                 ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`;
                 ctx.fillRect(i, j, 1, 1);
