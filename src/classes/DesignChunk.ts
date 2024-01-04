9a7d6e883b39a9cd1ceb3b1bd021fa68bfc43ed8
Megaemce <1651451+Megaemce@users.noreply.github.com>
2024-01-04 20:58:46 +0700

    remove unsued hight from designchunk

--- src/classes/DesignChunk.ts
+++ src/classes/DesignChunk.ts
@@ -2,7 +2,7 @@ import IChunk from "../interfaces/IChunk";
 import { ChunkTag } from "../types/ChunkTag";
 
 /**
- * Represents a design chunk with tag, coordinates, and height information.
+ * Represents a design chunk with tag and coordinates.
  */
 
 export default class DesignChunk implements IChunk {
@@ -12,20 +12,16 @@ export default class DesignChunk implements IChunk {
     x: number = 0;
     /** The y-coordinate of the design chunk. */
     y: number = 0;
-    /** The height information of the design chunk. */
-    height: number = 0;
 
     /**
      * Creates an instance of DesignChunk.
      * @param tag - The tag associated with the design chunk.
      * @param x - The x-coordinate of the design chunk.
      * @param y - The y-coordinate of the design chunk.
-     * @param height - The height information of the design chunk.
      */
     constructor(tag: ChunkTag, x: number, y: number, height: number = 0) {
         this.tag = tag;
         this.x = x;
         this.y = y;
-        this.height = height;
     }
 }
