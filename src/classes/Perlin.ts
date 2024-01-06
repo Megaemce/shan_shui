4c2acdc816028b73ae84df824578497f2753a172
Megaemce <1651451+Megaemce@users.noreply.github.com>
2024-01-06 15:08:25 +0700

    reverse Perlin to previous version

--- src/classes/Perlin.ts
+++ src/classes/Perlin.ts
@@ -32,58 +32,54 @@ export default class Perlin {
             }
         }
 
-        let [xInt, yInt, zInt] = [x, y, z].map(Math.floor);
-        let [xDec, yDec, zDec] = [x, y, z].map((val) => val - Math.floor(val));
-
-        const xDecRad = scaledCosine(xDec);
-        const yDecRad = scaledCosine(yDec);
-
-        let result = 0;
-        let amplifier = 0.5;
+        let xi = Math.floor(x);
+        let yi = Math.floor(y);
+        let zi = Math.floor(z);
+        let xf = x - xi;
+        let yf = y - yi;
+        let zf = z - zi;
+        let r = 0;
+        let ampl = 0.5;
 
         for (let o = 0; o < PERLIN_OCTAVES; o++) {
-            let of = xInt + (yInt << PERLIN_YWRAPB) + (zInt << PERLIN_ZWRAPB);
-            let noise1 = this.perlin[of & PERLIN_SIZE];
-            let noise2 = this.perlin[(of + PERLIN_YWRAP) & PERLIN_SIZE];
-            let noise3 =
-                this.perlin[(of + PERLIN_YWRAP + PERLIN_ZWRAP) & PERLIN_SIZE];
-
-            noise1 += xDecRad * (this.perlin[(of + 1) & PERLIN_SIZE] - noise1);
-            noise2 +=
-                xDecRad *
-                (this.perlin[(of + PERLIN_YWRAP + 1) & PERLIN_SIZE] - noise2);
-            noise1 += yDecRad * (noise2 - noise1);
+            let of = xi + (yi << PERLIN_YWRAPB) + (zi << PERLIN_ZWRAPB);
+            const rxf = scaledCosine(xf);
+            const ryf = scaledCosine(yf);
+            let n1 = this.perlin[of & PERLIN_SIZE];
+            n1 += rxf * (this.perlin[(of + 1) & PERLIN_SIZE] - n1);
+            let n2 = this.perlin[(of + PERLIN_YWRAP) & PERLIN_SIZE];
+            n2 +=
+                rxf * (this.perlin[(of + PERLIN_YWRAP + 1) & PERLIN_SIZE] - n2);
+            n1 += ryf * (n2 - n1);
             of += PERLIN_ZWRAP;
-            noise2 = this.perlin[of & PERLIN_SIZE];
-            noise2 += xDecRad * (this.perlin[(of + 1) & PERLIN_SIZE] - noise2);
-            noise3 +=
-                xDecRad *
-                (this.perlin[(of + PERLIN_YWRAP + 1) & PERLIN_SIZE] - noise3);
-            noise2 += yDecRad * (noise3 - noise2);
-            noise1 += scaledCosine(zDec) * (noise2 - noise1);
-
-            result += noise1 * amplifier;
-            amplifier *= PERLIN_AMP_FALLOFF;
-            xInt <<= 1;
-            xDec *= 2;
-            yInt <<= 1;
-            yDec *= 2;
-            zInt <<= 1;
-            zDec *= 2;
-
-            if (xDec >= 1.0) {
-                xInt++;
-                xDec--;
+            n2 = this.perlin[of & PERLIN_SIZE];
+            n2 += rxf * (this.perlin[(of + 1) & PERLIN_SIZE] - n2);
+            let n3 = this.perlin[(of + PERLIN_YWRAP) & PERLIN_SIZE];
+            n3 +=
+                rxf * (this.perlin[(of + PERLIN_YWRAP + 1) & PERLIN_SIZE] - n3);
+            n2 += ryf * (n3 - n2);
+            n1 += scaledCosine(zf) * (n2 - n1);
+            r += n1 * ampl;
+            ampl *= PERLIN_AMP_FALLOFF;
+            xi <<= 1;
+            xf *= 2;
+            yi <<= 1;
+            yf *= 2;
+            zi <<= 1;
+            zf *= 2;
+            if (xf >= 1.0) {
+                xi++;
+                xf--;
             }
-            if (yDec >= 1.0) {
-                yInt++;
-                yDec--;
+            if (yf >= 1.0) {
+                yi++;
+                yf--;
             }
-            if (zDec >= 1.0) {
-                zInt++;
-                zDec--;
+            if (zf >= 1.0) {
+                zi++;
+                zf--;
             }
         }
-        return result;
+        return r;
     }
 }
