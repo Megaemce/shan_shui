7473253d61803f177f1c30463e8f2ed460edfa48
Megaemce <1651451+Megaemce@users.noreply.github.com>
2024-01-22 13:52:07 +0700

    add proper var names

--- src/classes/elements/Decoration.ts
+++ src/classes/elements/Decoration.ts
@@ -8,8 +8,8 @@ import { lineDivider } from "../../utils/polytools";
  * @param {Point} [upperRightPoint=new Point(0, 100)] - The upper-right point of the bounding box.
  * @param {Point} [bottomLeftPoint=new Point(100, 0)] - The lower-left point of the bounding box.
  * @param {Point} [bottomRightPoint=new Point(100, 100)] - The lower-right point of the bounding box.
- * @param {number[]} [hsp=[1, 3]] - The horizontal subdivision parameters.
- * @param {number[]} [vsp=[1, 2]] - The vertical subdivision parameters.
+ * @param {number} [horizontalSubPoints=3] - The horizontal sub points number.
+ * @param {number} [verticalSubPoints=2] - The vertical sub points number.
  * @returns {Point[][]} An array of points representing the decorative elements.
  */
 export default function Decoration(
@@ -18,57 +18,91 @@ export default function Decoration(
     upperRightPoint: Point = new Point(0, 100),
     bottomLeftPoint: Point = new Point(100, 0),
     bottomRightPoint: Point = new Point(100, 100),
-    hsp: number[] = [1, 3],
-    vsp: number[] = [1, 2]
+    horizontalSubPoints: number = 3,
+    verticalSubPoints: number = 2
 ): Point[][] {
     const pointArray: Point[][] = [];
-    const dl = lineDivider([upperLeftPoint, bottomLeftPoint], vsp[1]);
-    const dr = lineDivider([upperRightPoint, bottomRightPoint], vsp[1]);
-    const du = lineDivider([upperLeftPoint, upperRightPoint], hsp[1]);
-    const dd = lineDivider([bottomLeftPoint, bottomRightPoint], hsp[1]);
+    const leftLine = lineDivider(
+        [upperLeftPoint, bottomLeftPoint],
+        verticalSubPoints
+    );
+    const rightLine = lineDivider(
+        [upperRightPoint, bottomRightPoint],
+        verticalSubPoints
+    );
+    const upperLine = lineDivider(
+        [upperLeftPoint, upperRightPoint],
+        horizontalSubPoints
+    );
+    const bottomLine = lineDivider(
+        [bottomLeftPoint, bottomRightPoint],
+        horizontalSubPoints
+    );
 
-    if (style === 1) {
+    if (style === 0) {
         // -| |-
-        const mlu = du[hsp[0]];
-        const mru = du[du.length - 1 - hsp[0]];
-        const mld = dd[hsp[0]];
-        const mrd = dd[du.length - 1 - hsp[0]];
+        const middleLeftUpper = upperLine[1];
+        const middleRightUpper = upperLine[upperLine.length - 2];
+        const middleLeftBottom = bottomLine[1];
+        const middleRightBottom = bottomLine[upperLine.length - 2];
 
-        for (let i = vsp[0]; i < dl.length - vsp[0]; i += vsp[0]) {
-            const mml = lineDivider([mlu, mld], vsp[1])[i];
-            const mmr = lineDivider([mru, mrd], vsp[1])[i];
-            const ml = dl[i];
-            const mr = dr[i];
-            pointArray.push(lineDivider([mml, ml], 5));
-            pointArray.push(lineDivider([mmr, mr], 5));
+        for (let i = 1; i < leftLine.length - 1; i++) {
+            const nextLeft = leftLine[i];
+            const nextRight = rightLine[i];
+            const middleLeft = lineDivider(
+                [middleLeftUpper, middleLeftBottom],
+                verticalSubPoints
+            )[i];
+            const middleRight = lineDivider(
+                [middleRightUpper, middleRightBottom],
+                verticalSubPoints
+            )[i];
+
+            pointArray.push(lineDivider([middleLeft, nextLeft], 5));
+            pointArray.push(lineDivider([middleRight, nextRight], 5));
         }
-        pointArray.push(lineDivider([mlu, mld], 5));
-        pointArray.push(lineDivider([mru, mrd], 5));
-    } else if (style === 2) {
+
+        pointArray.push(lineDivider([middleLeftUpper, middleLeftBottom], 5));
+        pointArray.push(lineDivider([middleRightUpper, middleRightBottom], 5));
+    } else if (style === 1) {
         // ||||
-        for (let i = hsp[0]; i < du.length - hsp[0]; i += hsp[0]) {
-            const mu = du[i];
-            const md = dd[i];
-            pointArray.push(lineDivider([mu, md], 5));
+        for (let i = 1; i < upperLine.length - 1; i++) {
+            const nextUpper = upperLine[i];
+            const nextBottom = bottomLine[i];
+
+            pointArray.push(lineDivider([nextUpper, nextBottom], 5));
         }
-    } else if (style === 3) {
+    } else if (style === 2) {
         // |##|
-        const mlu = du[hsp[0]];
-        const mru = du[du.length - 1 - hsp[0]];
-        const mld = dd[hsp[0]];
-        const mrd = dd[du.length - 1 - hsp[0]];
+        const middleLeftUpper = upperLine[1];
+        const middleRightUpper = upperLine[upperLine.length - 2];
+        const middleLeftBottom = bottomLine[1];
+        const middleRightBottom = bottomLine[upperLine.length - 2];
 
-        for (let i = vsp[0]; i < dl.length - vsp[0]; i += vsp[0]) {
-            const mml = lineDivider([mlu, mld], vsp[1])[i];
-            const mmr = lineDivider([mru, mrd], vsp[1])[i];
-            const mmu = lineDivider([mlu, mru], vsp[1])[i];
-            const mmd = lineDivider([mld, mrd], vsp[1])[i];
+        for (let i = 1; i < leftLine.length - 1; i++) {
+            const middleLeft = lineDivider(
+                [middleLeftUpper, middleLeftBottom],
+                verticalSubPoints
+            )[i];
+            const middleRight = lineDivider(
+                [middleRightUpper, middleRightBottom],
+                verticalSubPoints
+            )[i];
+            const middleUpper = lineDivider(
+                [middleLeftUpper, middleRightUpper],
+                verticalSubPoints
+            )[i];
+            const middleBottom = lineDivider(
+                [middleLeftBottom, middleRightBottom],
+                verticalSubPoints
+            )[i];
 
-            pointArray.push(lineDivider([mml, mmr], 5));
-            pointArray.push(lineDivider([mmu, mmd], 5));
+            pointArray.push(lineDivider([middleLeft, middleRight], 5));
+            pointArray.push(lineDivider([middleUpper, middleBottom], 5));
         }
-        pointArray.push(lineDivider([mlu, mld], 5));
-        pointArray.push(lineDivider([mru, mrd], 5));
+
+        pointArray.push(lineDivider([middleLeftUpper, middleLeftBottom], 5));
+        pointArray.push(lineDivider([middleRightUpper, middleRightBottom], 5));
     }
     return pointArray;
 }
