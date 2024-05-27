3210c58be50b77c2cc46c7bd097351824a0ccda5
Megaemce <1651451+Megaemce@users.noreply.github.com>
2024-05-27 20:50:21 +0700

    create water layer again

--- src/classes/layers/WaterLayer.ts
+++ src/classes/layers/WaterLayer.ts
@@ -1,38 +1,36 @@
+import Layer from "../Layer";
 import PRNG from "../PRNG";
 import Perlin from "../Perlin";
 import Point from "../Point";
 import Stroke from "../elements/Stroke";
 import { config } from "../../config";
-import Structure from "../Structure";
 
 const COLORNOALFA = config.layers.water.colorNoAlfa;
-const DEFAULT_HEIGTH = config.layers.water.defaultHeight;
 const DEFAULT_WAVECLUSTERS = config.layers.water.defaultWaveClusters;
-const DEFAULT_WIDTH = config.layers.water.defaultWidth;
 
 /**
  * Class representing a water chunk with undulating waves.
  *
- * @extends Strcuture
+ * @extends Layer
  */
-export default class MiddleMountainWater extends Structure {
+export default class WaterLayer extends Layer {
     /**
      * Creates an instance of WaterChunk.
      * @param {number} xOffset - X-coordinate offset for the chunk.
      * @param {number} yOffset - Y-coordinate offset for the chunk.
-     * @param {number} [width=DEFAULT_WIDTH] - Width of the waves.
-     * @param {number} [height=DEFAULT_HEIGTH] - Height of the waves.
+     * @param {number} [width] - Width of the waves.
+     * @param {number} [height] - Height of the waves.
      * @param {number} [waveClusters=DEFAULT_WAVECLUSTERS] - Number of clusters of waves.
      */
 
     constructor(
         xOffset: number,
         yOffset: number,
-        width: number = DEFAULT_WIDTH,
-        height: number = DEFAULT_HEIGTH,
+        width: number,
+        height: number,
         waveClusters: number = DEFAULT_WAVECLUSTERS
     ) {
-        super();
+        super("water", xOffset, yOffset);
 
         const resolution = 5;
         let yk = 0;
